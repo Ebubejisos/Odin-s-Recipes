ad9ae69bc2daf58fb3e52ebecaef8a1fcd1b4127
Ebubejisos <anosikegideon007@gmail.com>
2022-11-21 15:36:20 +0100

    Update index.js
    wrote a 5 round console game with pre-declared player hand as Rock

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function getComputerChoice() {
-    
+    const handof = ['ROCK','PAPER','SCISSORS'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return handof[randomNum];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection){
+        return 'Its a tie'
+    }
+    else if(playerSelection && computerSelection == 'PAPER'){
+        return "You Lose! Paper beats Rock";
+    }
+    else{
+        return "You Win! Rock beats Scissors";
+    }
+    }
+
+    const game = ()=>{
+        for(let i = 0; i < 5; i++){
+            let playerSelection = "roCk";
+            playerSelection = playerSelection.toUpperCase();
+            const computerSelection = getComputerChoice();
+            console.log(playRound(playerSelection, computerSelection)); 
+        }
+    }
+
+
+    game();
