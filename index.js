14df08f1adde5cbf200a7c9d5607a7b4aa8782c4
Ebubejisos <anosikegideon007@gmail.com>
2022-11-22 16:49:59 +0100

    Update index.js
    added user to input hand "Rock" with windows.prompt

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let player = 0;
+let computer = 0;
+
 function getComputerChoice() {
     const handof = ['ROCK','PAPER','SCISSORS'];
     const randomNum = Math.floor(Math.random() * 3);
@@ -6,24 +9,36 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection){
-        return 'Its a tie'
+        return 'Draw'
     }
     else if(playerSelection && computerSelection == 'PAPER'){
+        computer++
         return "You Lose! Paper beats Rock";
     }
     else{
+        player++
         return "You Win! Rock beats Scissors";
     }
-    }
+}
 
     const game = ()=>{
         for(let i = 0; i < 5; i++){
-            let playerSelection = "roCk";
-            playerSelection = playerSelection.toUpperCase();
             const computerSelection = getComputerChoice();
             console.log(playRound(playerSelection, computerSelection)); 
         }
+        console.log(`Player ${player} : ${computer} Computer`);
+        if(player > computer){
+            console.log("You Win!");
+        }
+        else if(player == computer){
+            console.log("It's a tie!")
+        }
+        else{
+            console.log("You Lost!")
+        }
     }
-
-
+    
+    let playerSelection = window.prompt('input hand');
+    playerSelection = playerSelection.toUpperCase();
+    
     game();
